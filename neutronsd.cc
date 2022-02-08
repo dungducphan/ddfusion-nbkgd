5b23cad8e85d1966239545fb19cb8849da1de6e7
dungducphan <brianp.dung@utexas.edu>
2022-02-07 18:11:43 -0600

    Adding the 3D position of the neutrons detected on the SD to form an angular dependence of the neutrons.

--- neutronsd.cc
+++ neutronsd.cc
@@ -15,9 +15,15 @@ G4bool NeutronSD::ProcessHits(G4Step * aStep, G4TouchableHistory *) {
   G4StepPoint *prePoint = aStep->GetPreStepPoint();
 
   G4double energy = prePoint->GetKineticEnergy();
+  G4double x_hit = prePoint->GetPosition().getX();
+  G4double y_hit = prePoint->GetPosition().getY();
+  G4double z_hit = prePoint->GetPosition().getZ();
 
   G4AnalysisManager* man = G4AnalysisManager::Instance();
   man->FillNtupleDColumn(0, energy / MeV);
+  man->FillNtupleDColumn(1, x_hit / m);
+  man->FillNtupleDColumn(2, y_hit / m);
+  man->FillNtupleDColumn(3, z_hit / m);
   man->AddNtupleRow(0);
 
   track->SetTrackStatus(fStopAndKill);
