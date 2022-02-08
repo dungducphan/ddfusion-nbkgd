7dfe96d78e6eb9d6ae20ff84c32b52f41a13490c
dungducphan <brianp.dung@utexas.edu>
2022-02-07 18:10:15 -0600

    Using exponential spectrum for the kinetic energy of deuteriums. The excitation energy will be change to see the effect of background neutrons energies.

--- generator.cc
+++ generator.cc
@@ -5,24 +5,33 @@ MyPrimaryGenerator::MyPrimaryGenerator() {
 
   // First source: excited deuterium nuclei
   // ------------------------------------------------------------------------------------------------------------------------
-  G4SingleParticleSource* exDeuteriumIon = fGeneralParticleSource->GetCurrentSource();
-  G4ParticleDefinition* exDIon = G4ParticleTable::GetParticleTable()->FindParticle("e-");
+  G4SingleParticleSource *exDeuteriumIon =
+      fGeneralParticleSource->GetCurrentSource();
+  G4ParticleDefinition *exDIon =
+      G4ParticleTable::GetParticleTable()->FindParticle("e-");
 
   exDeuteriumIon->SetParticleDefinition(exDIon);
-  exDeuteriumIon->SetNumberOfParticles(1);
-  
-  exDeuteriumIon->GetPosDist()->SetPosDisType("Volume"); // Point, Beam, Plane, Surface, Volume
-  exDeuteriumIon->GetPosDist()->SetPosDisShape("Sphere"); // Square, Circle, Ellipse, Rectangle, Sphere, Ellipsoid, Cylinder, Parallelepiped
-  exDeuteriumIon->GetPosDist()->SetRadius(3*um);
-  exDeuteriumIon->GetPosDist()->SetCentreCoords(G4ThreeVector(0., 0., 10. * cm));
+  exDeuteriumIon->SetNumberOfParticles(10000);
+
+  exDeuteriumIon->GetPosDist()->SetPosDisType(
+      "Volume"); // Point, Beam, Plane, Surface, Volume
+  exDeuteriumIon->GetPosDist()->SetPosDisShape(
+      "Sphere"); // Square, Circle, Ellipse, Rectangle, Sphere, Ellipsoid,
+                 // Cylinder, Parallelepiped
+  exDeuteriumIon->GetPosDist()->SetRadius(3 * um);
+  exDeuteriumIon->GetPosDist()->SetCentreCoords(
+      G4ThreeVector(0., 0., 10. * cm));
   // exDeuteriumIon->GetPosDist()->SetPosRot1(G4ThreeVector(0., 0., 0.));
   // exDeuteriumIon->GetPosDist()->SetPosRot2(G4ThreeVector(0., 0., 0.));
   exDeuteriumIon->GetPosDist()->ConfineSourceToVolume("NULL");
 
-  exDeuteriumIon->GetAngDist()->SetAngDistType("iso"); // Isotropic, Cosine-law, Beam, User-defined
-  exDeuteriumIon->GetAngDist()->DefineAngRefAxes("angref1", G4ThreeVector(1, 0, 0));
-  exDeuteriumIon->GetAngDist()->DefineAngRefAxes("angref2", G4ThreeVector(1, 1, 0));
- 
+  exDeuteriumIon->GetAngDist()->SetAngDistType(
+      "iso"); // Isotropic, Cosine-law, Beam, User-defined
+  exDeuteriumIon->GetAngDist()->DefineAngRefAxes("angref1",
+                                                 G4ThreeVector(1, 0, 0));
+  exDeuteriumIon->GetAngDist()->DefineAngRefAxes("angref2",
+                                                 G4ThreeVector(1, 1, 0));
+
   exDeuteriumIon->GetAngDist()->SetMinTheta(0);
   exDeuteriumIon->GetAngDist()->SetMaxTheta(TMath::Pi());
   exDeuteriumIon->GetAngDist()->SetMinPhi(0);
@@ -33,18 +42,21 @@ MyPrimaryGenerator::MyPrimaryGenerator() {
   // exDeuteriumIon->GetAngDist()->SetMinPhi(TMath::Pi()/2. - 0.01);
   // exDeuteriumIon->GetAngDist()->SetMaxPhi(TMath::Pi()/2. + 0.01);
 
-  exDeuteriumIon->GetEneDist()->SetEnergyDisType("Exp"); // Mono, Lin, Pow, Exp, Gaus, Brem, BBody, Cdg (cosmic diffuse gamma), User, Arb, Epn (energy per nucleon)
-  exDeuteriumIon->GetEneDist()->SetEzero(4.343 * MeV); // I~Exp(-E/E0)                                                  
+  exDeuteriumIon->GetEneDist()->SetEnergyDisType(
+      "Exp"); // Mono, Lin, Pow, Exp, Gaus, Brem, BBody, Cdg (cosmic diffuse
+              // gamma), User, Arb, Epn (energy per nucleon)
+  exDeuteriumIon->GetEneDist()->SetEzero(4.343 * MeV); // I~Exp(-E/E0)
   // ------------------------------------------------------------------------------------------------------------------------
 }
 
-MyPrimaryGenerator::~MyPrimaryGenerator() {
-  delete fGeneralParticleSource;
-}
+MyPrimaryGenerator::~MyPrimaryGenerator() { delete fGeneralParticleSource; }
 
-void MyPrimaryGenerator::GeneratePrimaries(G4Event* anEvent) {
-  if (fGeneralParticleSource->GetCurrentSource()->GetParticleDefinition() == G4ParticleTable::GetParticleTable()->FindParticle("e-")) {
-    G4ParticleDefinition* exDIon = G4ParticleTable::GetParticleTable()->GetIonTable()->GetIon(1, 2, 2 * MeV);
+void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent) {
+  if (fGeneralParticleSource->GetCurrentSource()->GetParticleDefinition() ==
+      G4ParticleTable::GetParticleTable()->FindParticle("e-")) {
+    G4ParticleDefinition *exDIon =
+        G4ParticleTable::GetParticleTable()->GetIonTable()->GetIon(1, 2,
+                                                                   1.5 * MeV);
     fGeneralParticleSource->GetCurrentSource()->SetParticleDefinition(exDIon);
   }
   fGeneralParticleSource->GeneratePrimaryVertex(anEvent);
